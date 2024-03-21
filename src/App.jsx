04e25c8e6d83e4ebf6e82252b67b2f1ff8113d2f
WeronikaCzepiela <weronika.czepiela@gmail.com>
2024-03-21 21:54:34 +0100

    add title

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="w-screen h-screen bg-gray-100"></div>;
+  return (
+    <div className="w-screen h-screen bg-gray-100">
+      <div>Chemical Instalation</div>
+    </div>
+  );
 }
 
 export default App;
