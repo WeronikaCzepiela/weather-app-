4e930aa5f1779e288ed1207ecbc371a041a32cea
WeronikaCzepiela <weronika.czepiela@gmail.com>
2024-03-22 14:31:45 +0100

    repair title

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="w-screen h-screen bg-gray-100">
-      <div>Chemical Instalation</div>
+      <div>Chemical Installation</div>
     </div>
   );
 }
