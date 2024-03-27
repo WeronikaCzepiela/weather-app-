6d9ba00c9c30df2908218747a47682d387253041
WeronikaCzepiela <weronika.czepiela@gmail.com>
2024-03-27 22:05:41 +0100

    add layout for title

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-gray-100">
+    <div className="w-screen h-screen bg-gray-100 text-3lg text-white">
       <div>Chemical Installation</div>
     </div>
   );
