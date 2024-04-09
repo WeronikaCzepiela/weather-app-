fdd12cea2d0de086fb31b74c3b1a96228107e078
WeronikaCzepiela <weronika.czepiela@gmail.com>
2024-04-09 23:04:36 +0200

    add new element to main pag e

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="w-screen h-screen bg-gray-100">
       <div className="text-3lg text-white">Chemical Installation</div>
+      <div></div>
     </div>
   );
 }
